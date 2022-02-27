fa8f81b3638d95f47570fb276eb9ab9c098682fe
Sarah Hart <sarah.hart.nc@gmail.com>
2022-02-27 03:16:33 -0500

    start button event listener

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,4 +1,10 @@
+const startButton = document.getElementById('start-btn')
 
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    startButton.classList.add('hide')
+}
 
 //questions
 var questions = [
