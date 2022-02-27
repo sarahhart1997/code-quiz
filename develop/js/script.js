354e170b00e8db36e164d9fbfb5cb33e0504938e
Sarah Hart <sarah.hart.nc@gmail.com>
2022-02-27 01:57:02 -0500

    add the questions to the js file

---
+++ develop/js/script.js
@@ -0,0 +1,38 @@
+
+
+//questions
+var questions = [
+    {
+        question:"Commonly used data types do NOT include:",
+        choices:["strings", "booleans", "alerts", "numbers"],
+        correct:"alerts"
+    },
+    {
+        question:"The condition in an if/else statement is enclosed with ________",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correct: "parenthesis"
+    },
+    {
+        question:"Arrays in JavaScript can be used to store _____.", 
+        answers:["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct:"all of the above"
+    },
+    {
+        question:"String values must be enclosed within _____ when being assigned to variables.",
+        answers:["commas", "curly brackets", "quotes", "parenthesis"],
+        correct:"quotes"
+    },
+    {
+        question:"A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers:["JavaScript", "termial/bash", "for loops", "console.log"],
+        correct:"console.log"
+    }
+];
+
+//loops through the questions
+
+//subtracts for incorrect answers
+
+//keeps timer from counting in the negative
+
+//submit the score
